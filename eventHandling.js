b2ee12ca5c4174250da02934defb1e8a5a86c530
Sean Duffy <seanduffy38@gmail.com>
2023-02-15 05:43:18 -0500

    only need right column

--- eventHandling.js
+++ eventHandling.js
@@ -1,12 +1,22 @@
+console.log("JavaScript code is running")
+
 const data = [[1, 2], [2, 4], [6, 2], [9, 9]];
 const w = 500;
 const h = 500;
-const svg = d3.select("body")
-              .append("svg")
-              .attr("width", w)
-              .attr("height", h);
+const svg = d3.select(".scatter");
+
+const xScale = d3.scaleLinear()
+  .domain([0, 10])
+  .range([0, 250]);
+
+const yScale = d3.scaleLinear()
+  .domain([0, 10])
+  .range([250, 0]);
               
 svg.selectAll("circle")
    .data(data)
    .enter()
    .append("circle")
+   .attr("cx", d => xScale(d[0]))
+   .attr("cy", d => yScale(d[1]))
+   .attr("r", 5);
